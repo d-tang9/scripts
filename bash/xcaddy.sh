9ea71d37e12b79798ce53651499808415cd5e212
d-tang9 <104226020+d-tang9@users.noreply.github.com>
2024-09-13 17:43:28 -0400

    Update xcaddy.sh

--- bash/xcaddy.sh
+++ bash/xcaddy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-xcaddy-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-xcaddy.list
